2c2ed54f42fb7e0a430981f2283a230e37c68eb4
cheung king yin <kycheung@chowsangsang.com>
2019-03-04 14:26:55 +0800

    Add echo and echo endless function in both client and server

--- index.js
+++ index.js
@@ -2,10 +2,41 @@ const WebSocket = require('ws')
 // const wss = new WebSocket.Server({ port: 8080 })
 const wss = new WebSocket.Server({ port: 3000 })
 
+let EchoCounter = 0
+
+function echoToClient(ws, JsonData) {
+  ws.send(JsonData.msg)
+}
+
+function echoToClientN(ws, JsonData) {
+  setInterval(() => {
+    ws.send(`Rely${EchoCounter}: ${JsonData.msg}`)
+    EchoCounter += 1
+    if (EchoCounter > 999999) {
+      EchoCounter = 0
+    }
+  }, JsonData.N)
+}
+
+
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
-    console.log('received: %s', message)
+    // console.log('received: %s', message)
+    try {
+      const JsonData = JSON.parse(message)
+      console.log(JsonData)
+      if (JsonData.action === 'echo') {
+        echoToClient(ws, JsonData)
+      }
+
+      if (JsonData.action === 'echoN') {
+        echoToClientN(ws, JsonData)
+      }
+
+    } catch (e) {
+      console.log(e)
+    }
   })
   // ws.send('something')
-  setInterval(() => { ws.send('something testing') }, 100)
+  // setInterval(() => { ws.send('something testing') }, 100)
 })
